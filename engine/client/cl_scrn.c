9a710123f9df1bef88b40c265d2f95c65f3d0038
maximqad <craf7y24@gmail.com>
2025-06-04 17:03:56 +0300

    engine: client: do not replace default fonts

--- engine/client/cl_scrn.c
+++ engine/client/cl_scrn.c
@@ -720,6 +720,7 @@ void SCR_LoadCreditsFont( void )
 	float scale = hud_fontscale.value;
 	uint32_t crc = 0;
 
+#if !XASH_DREAMCAST
 	// replace default gfx.wad textures by current charset's font
 	if( !CRC32_File( &crc, "gfx.wad" ) || crc == 0x49eb9f16 )
 	{
@@ -735,7 +736,7 @@ void SCR_LoadCreditsFont( void )
 
 	if( !success )
 		success = Con_LoadVariableWidthFont( "gfx/creditsfont.fnt", font, scale, &hud_fontrender, TF_FONT );
-
+#endif
 	if( !success )
 		success = Con_LoadFixedWidthFont( "gfx/conchars", font, scale, &hud_fontrender, TF_FONT );
 
