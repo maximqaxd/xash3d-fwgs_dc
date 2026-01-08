1c1b0071a5998403b7f8040ee5de221086984c42
maximqad <craf7y24@gmail.com>
2026-01-08 15:57:00 +0300

    engine: platform: dreamcast: raise vertex buffer

--- engine/platform/dreamcast/vid_dc.c
+++ engine/platform/dreamcast/vid_dc.c
@@ -27,7 +27,7 @@ static qboolean vsync;
 
 pvr_init_params_t params = {
 	{ PVR_BINSIZE_8, PVR_BINSIZE_0, PVR_BINSIZE_8, PVR_BINSIZE_0, PVR_BINSIZE_8 },
-	3072 * 256,    /* vertex buffer */
+	4096 * 256,    /* vertex buffer */
 	0,             /* dma disabled for TA  */
 	0,             /* fsaa off */
 	0,             /* keep PVR translucent autosort OFF  */
